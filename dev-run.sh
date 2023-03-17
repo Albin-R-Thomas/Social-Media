c6f1d929b4ca10e6dc7f65f9cdf69b5b1da17bc0
Albin-R-Thomas <albinthomas130202.at@gmail.com>
2023-03-17 19:39:51 +0530

    updated dev-run.sh

--- dev-run.sh
+++ dev-run.sh
@@ -2,12 +2,12 @@ cd app
 
 
 mv package.json package.json.bak
-mv package-lock.yaml package-lock.yaml.bak
+mv package-lock.json package-lock.json.bak
 npm install prisma@4.8.0 --no-lockfile
 npm install @prisma/client --no-lockfile
 npm prisma generate --schema ./prisma/schema.prisma
 mv package.json.bak package.json
-mv npm-lock.yaml.bak npm-lock.yaml
+mv package-lock.json.bak pakage-lock.json
 
 npm install
 
