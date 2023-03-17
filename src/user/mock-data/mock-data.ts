23f2b1864a7895229717375efb73fe791aedd669
Albin-R-Thomas <albinthomas130202.at@gmail.com>
2023-03-17 16:16:10 +0530

    feat: added testcases for user and post modules

---
+++ src/user/mock-data/mock-data.ts
@@ -0,0 +1,10 @@
+export const mockUser = {
+  data: {
+    email: undefined,
+    name: undefined,
+    password: '$2a$10$LJHeP1.ZAHT6UK7bTQh5R.tXNzViEoT7fX.U/7oojW/ApVcJ/257i',
+    token:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjA4ODg3ODg0LTE0YWQtNGM3MC04ZTI1LWNhMDNlNmJhZmRkZCIsIm5hbWUiOiJBbGJpbiIsImlhdCI6MTY3ODk5Nzc4M30.GvEZw97N5jJ_n7vKtV0iLCZbcC6mq-v6JQZxR-xRIso',
+    secret: 'sdkl1(*&$#(*23',
+  },
+};
